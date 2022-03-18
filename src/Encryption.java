c747abf4fe048b9f2ef8b792decc40394fd32764
Michelle Liu <michellel246@nycstudents.net>
2022-03-18 01:33:14 -0400

    finished encryption and encryptionrunner class. testing works: all passed!

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,15 +1,20 @@
+import java.util.Arrays;
 public class Encryption {
     private int shiftLet;
     private int shiftRow;
     private int shiftCol;
+    private final String upperCase;
+    private final String lowerCase;
 
     public Encryption(int shiftLet, int shiftRow, int shiftCol) {
         this.shiftLet = shiftLet;
         this.shiftRow = shiftRow;
         this.shiftCol = shiftCol;
+        upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        lowerCase = "abcdefghijklmnopqrstuvwxyz";
     }
 
-    public String encryptor(String message) {
+    public int rowsNeed(String message) {
         int rows = 0;
         for (int i = 2; i != message.length(); i++) {
             if (message.length() % i == 0) {
@@ -17,60 +22,68 @@ public class Encryption {
                 break;
             }
         }
-        int cols = 0;
         if (rows == 0) {
-            rows = message.length() / 3;
-            cols = 3;
-        } else {
-            cols = message.length() / rows;
+            rows = 1;
         }
-        String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        return rows;
+    }
+
+    public String encryptor(String message) {
+        int rows = rowsNeed(message);
+        int cols = message.length() / rows;
         String[][] temp = new String[rows][cols];
         int index = 0;
         for (int row = 0; row != rows; row++) {
             for (int col = 0; col != cols; col++) {
-                if (index < message.length()) {
-                    String letter = message.substring(index, index + 1);
-                    boolean uppercaseBefore = upperCase.indexOf(letter) > -1;
-                    String newLetter = letter.toUpperCase();
-                    int moved = upperCase.indexOf(newLetter) + shiftLet;
-                    while (moved >= upperCase.length()) {
-                        moved -= upperCase.length();
-                    }
-                    if (uppercaseBefore) {
-                        newLetter = upperCase.substring(moved, moved + 1);
-                    }
-                    else {
-                        newLetter = lowerCase.substring(moved, moved + 1);
-                    }
-                    temp[row][col] = newLetter;
-                    index++;
-                } else {
-                    temp[row][col] = "=";
+                String letter = message.substring(index, index + 1);
+                boolean uppercaseBefore = upperCase.indexOf(letter) > -1;
+                String newLetter = letter.toUpperCase();
+                int moved = upperCase.indexOf(newLetter) + shiftLet;
+                while (moved >= upperCase.length()) {
+                    moved -= upperCase.length();
                 }
+                if (uppercaseBefore) {
+                    newLetter = upperCase.substring(moved, moved + 1);
+                }
+                else {
+                    newLetter = lowerCase.substring(moved, moved + 1);
+                }
+                temp[row][col] = newLetter;
+                index++;
             }
         }
+        /*System.out.println("After change in letter:");
+        for (String[] row: temp) {
+            System.out.println(Arrays.toString(row));
+        }*/
         String[][] temp2 = new String[rows][cols];
         for (int row = 0; row != rows; row++) {
             for (int col = 0; col != cols; col++) {
                 int moved = col + shiftCol;
-                while (shiftCol >= cols) {
+                while (moved >= cols) {
                     moved -= cols;
                 }
                 temp2[row][moved] = temp[row][col];
             }
         }
+        /*System.out.println("Change in col:");
+        for (String[] row: temp2) {
+            System.out.println(Arrays.toString(row));
+        }*/
         String[][] temp3 = new String[rows][cols];
         for (int col = 0; col != cols; col++) {
             for (int row = 0; row != rows; row++) {
                 int moved = row + shiftRow;
-                while (shiftRow >= rows) {
+                while (moved >= rows) {
                     moved -= rows;
                 }
-                temp3[moved][cols] = temp2[row][col];
+                temp3[moved][col] = temp2[row][col];
             }
         }
+        /*System.out.println("Change in row:");
+        for (String[] row: temp3) {
+            System.out.println(Arrays.toString(row));
+        }*/
         String encryptedWord = "";
         for (String[] row: temp3) {
             for (String letter: row) {
@@ -78,5 +91,70 @@ public class Encryption {
             }
         }
         return encryptedWord;
+    }
+
+    public String decryptor(String message) {
+        int rows = rowsNeed(message);
+        int cols = message.length() / rows;
+        String[][] temp = new String[rows][cols];
+        int index = 0;
+        for (int row = 0; row != rows; row++) {
+            for (int col = 0; col != cols; col++) {
+                String letter = message.substring(index, index + 1);
+                boolean uppercaseBefore = upperCase.indexOf(letter) > -1;
+                String newLetter = letter.toUpperCase();
+                int moved = upperCase.indexOf(newLetter) - shiftLet;
+                while (moved < 0) {
+                    moved += upperCase.length();
+                }
+                if (uppercaseBefore) {
+                    newLetter = upperCase.substring(moved, moved + 1);
+                }
+                else {
+                    newLetter = lowerCase.substring(moved, moved + 1);
+                }
+                temp[row][col] = newLetter;
+                index++;
+            }
+        }
+        /*System.out.println("After change in letter:");
+        for (String[] row: temp) {
+            System.out.println(Arrays.toString(row));
+        }*/
+        String[][] temp2 = new String[rows][cols];
+        for (int row = 0; row != rows; row++) {
+            for (int col = 0; col != cols; col++) {
+                int moved = col - shiftCol;
+                while (moved < 0) {
+                    moved += cols;
+                }
+                temp2[row][moved] = temp[row][col];
+            }
+        }
+        /*System.out.println("Change in col:");
+        for (String[] row: temp2) {
+            System.out.println(Arrays.toString(row));
+        }*/
+        String[][] temp3 = new String[rows][cols];
+        for (int col = 0; col != cols; col++) {
+            for (int row = 0; row != rows; row++) {
+                int moved = row - shiftRow;
+                while (moved < 0) {
+                    moved += rows;
+                }
+                temp3[moved][col] = temp2[row][col];
+            }
+        }
+        /*System.out.println("Change in row:");
+        for (String[] row: temp3) {
+            System.out.println(Arrays.toString(row));
+        }*/
+        String decryptedWord = "";
+        for (String[] row: temp3) {
+            for (String letter: row) {
+                decryptedWord += letter;
+            }
+        }
+        return decryptedWord;
     }
 }
---
+++ src/EncryptionRunner.java
@@ -0,0 +1,56 @@
+public class EncryptionRunner {
+    public static void main(String[] args) {
+        //Shows the process: change in let, col, row
+        //testing shift in col
+        Encryption test1 = new Encryption(0, 0, 2);
+        String ogWord1 = "abcdefghi";
+        String encrypted1 = test1.encryptor(ogWord1);
+        String decrypted1 = test1.decryptor(encrypted1);
+        System.out.println("Test 1 Word: " + ogWord1);
+        System.out.println("\nExpected Encrypted: bcaefdhig\nActual Encrypted: " + encrypted1 + "\n");
+        System.out.println("Expected Decrypted: " + ogWord1 + "\nActual Decrypted: " + decrypted1 + "\n");
+        if (ogWord1.equals(decrypted1) && encrypted1.equals("bcaefdhig")) { System.out.println("TEST PASSED"); }
+        else { System.out.println("TEST FAILED"); }
+        System.out.println();
+
+        //testing shift in col
+        Encryption test2 = new Encryption(0, 2, 0);
+        String ogWord2 = "abcdefghi";
+        String expected2 = "defghiabc";
+        String encrypted2 = test2.encryptor(ogWord2);
+        String decrypted2 = test2.decryptor(encrypted2);
+        System.out.println("\nTest 2 Word: " + ogWord2);
+        System.out.println("\nExpected Encrypted: " + expected2 + "\nActual Encrypted: " + encrypted2 + "\n");
+        System.out.println("Expected Decrypted: " + ogWord2 + "\nActual Decrypted: " + decrypted2 + "\n");
+        if (ogWord2.equals(decrypted2) && encrypted2.equals(expected2)) { System.out.println("TEST PASSED"); }
+        else { System.out.println("TEST FAILED"); }
+        System.out.println();
+
+        //testing change in letter
+        Encryption test3 = new Encryption(1, 0, 0);
+        String ogWord3 = "abcdefghi";
+        String expected3 = "bcdefghij";
+        String encrypted3 = test3.encryptor(ogWord3);
+        String decrypted3 = test3.decryptor(encrypted3);
+        System.out.println("\nTest 3 Word: " + ogWord3);
+        System.out.println("\nExpected Encrypted: " + expected3 + "\nActual Encrypted: " + encrypted3 + "\n");
+        System.out.println("Expected Decrypted: " + ogWord3 + "\nActual Decrypted: " + decrypted3 + "\n");
+        if (ogWord3.equals(decrypted3) && encrypted3.equals(expected3)) { System.out.println("TEST PASSED"); }
+        else { System.out.println("TEST FAILED"); }
+        System.out.println();
+
+        //testing different length
+        Encryption test4 = new Encryption(0, 1, 2);
+        String ogWord4 = "abcdefghij";
+
+        String expected4 = "ijfghdeabc";
+        String encrypted4 = test4.encryptor(ogWord4);
+        String decrypted4 = test4.decryptor(encrypted4);
+        System.out.println("\nTest 4 Word: " + ogWord4);
+        System.out.println("\nExpected Encrypted: " + expected4 + "\nActual Encrypted: " + encrypted4 + "\n");
+        System.out.println("Expected Decrypted: " + ogWord4 + "\nActual Decrypted: " + decrypted4 + "\n");
+        if (ogWord4.equals(decrypted4) && encrypted4.equals(expected4)) { System.out.println("TEST PASSED"); }
+        else { System.out.println("TEST FAILED"); }
+        System.out.println();
+    }
+}
